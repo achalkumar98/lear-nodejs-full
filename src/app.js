f7afaf2a1be49d7234bba5b462d67f0c017d40cd
achalkumar98 <hackerachal1620@gmail.com>
2025-04-10 15:32:25 +0530

    routes respond different paths

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+// Create a Server with Express.js
+// const express = require("express");
+// const app = express();
+// const port = 7777;
+
+// app.listen(port, () => {
+//   console.log("Server is runnning on port " + port);
+// });
+
+
+
+
+// Use Routes to respond to different paths and in express order will matter always
 const express = require("express");
 const app = express();
 const port = 7777;
@@ -15,5 +28,5 @@ app.use("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server is runnning on port " + port);
+    console.log("Server is runnning on port " + port);
 });
